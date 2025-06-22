1712ddc476e788cc7a8e06ead1d773f0ed78a82f
Mani-2308 <kodali.mani092@gmail.com>
2025-06-22 07:14:14 +0530

    first commit

---
+++ add user from.js
@@ -0,0 +1,27 @@
+let addUserFormEl = document.getElementById("addUserForm");
+
+let nameEl = document.getElementById("name");
+let nameErrMsgEl = document.getElementById("nameErrMsg");
+
+let emailEl = document.getElementById("email");
+let emailErrMsgEl = document.getElementById("emailErrMsg");
+
+addUserFormEl.addEventListener("submit", function(event) {
+    event.preventDefault();
+});
+
+nameEl.addEventListener("blur", function(event) {
+    if (event.target.value === "") {
+        nameErrMsgEl.textContent = "Required*";
+    } else {
+        nameErrMsgEl.textContent = "";
+    }
+});
+
+emailEl.addEventListener("blur", function(event) {
+    if (event.target.value === "") {
+        emailErrMsgEl.textContent = "Required*";
+    } else {
+        emailErrMsgEl.textContent = "";
+    }
+});
